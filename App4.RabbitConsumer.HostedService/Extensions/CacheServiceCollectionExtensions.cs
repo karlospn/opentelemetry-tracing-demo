39ed335fa12f8a84267217b18b6f27fc43486be1
carlos pons <karlospn@gmail.com>
2024-04-15 10:52:55 +0200

    Add ziggycreatures caching library

--- App4.RabbitConsumer.HostedService/Extensions/CacheServiceCollectionExtensions.cs
+++ App4.RabbitConsumer.HostedService/Extensions/CacheServiceCollectionExtensions.cs
@@ -1,22 +1,12 @@
 ﻿using System;
-using Microsoft.Extensions.DependencyInjection.Extensions;
-using StackExchange.Redis;
 using ZiggyCreatures.Caching.Fusion;
 using ZiggyCreatures.Caching.Fusion.Backplane.StackExchangeRedis;
 using ZiggyCreatures.Caching.Fusion.Serialization.SystemTextJson;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
-    public static class RedisCacheServiceCollectionExtensions
+    public static class CacheServiceCollectionExtensions
     {
-        public static void AddRedisConnectionMultiplexer(this IServiceCollection services,
-            string connectionString)
-        {
-            ArgumentException.ThrowIfNullOrWhiteSpace(connectionString);
-            var multiplexer = ConnectionMultiplexer.Connect(connectionString) as IConnectionMultiplexer;
-            services.TryAddSingleton(multiplexer);
-        }
-
         public static void AddRedisFusionCacheService(this IServiceCollection services,
             string connectionString,
             string instanceName)
