4fcb8441ce72bb0874aa30a856ab778f03fc823a
carlos pons <karlospn@gmail.com>
2024-04-15 10:02:08 +0200

    Update readis instrumentation

---
+++ App4.RabbitConsumer.HostedService/Extensions/RedisCacheServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using StackExchange.Redis;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class RedisCacheServiceCollectionExtensions
+    {
+        public static void AddRedisConnectionMultiplexer(this IServiceCollection services,
+            string connectionString)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(connectionString);
+            var multiplexer = ConnectionMultiplexer.Connect(connectionString) as IConnectionMultiplexer;
+            services.TryAddSingleton(multiplexer);
+        }
+    }
+}
--- App4.RabbitConsumer.HostedService/Program.cs
+++ App4.RabbitConsumer.HostedService/Program.cs
@@ -2,9 +2,11 @@ using System;
 using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.Caching.StackExchangeRedis;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
 using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
+using StackExchange.Redis;
 
 
 namespace App4.RabbitConsumer.HostedService
@@ -17,23 +19,24 @@ namespace App4.RabbitConsumer.HostedService
 
             builder.Services.AddHostedService<Worker>();
 
+            var redisConnString = $"{builder.Configuration["Redis:Host"]}:{builder.Configuration["Redis:Port"]}";
+
             builder.Services.AddStackExchangeRedisCache(options =>
             {
-                var connString =
-                    $"{builder.Configuration["Redis:Host"]}:{builder.Configuration["Redis:Port"]}";
-                options.Configuration = connString;
+                options.Configuration = redisConnString;
             });
+
+            builder.Services.AddRedisConnectionMultiplexer(redisConnString);
 
             builder.Services.AddOpenTelemetry().WithTracing(b =>
             {
-
                 b.AddAspNetCoreInstrumentation()
                     .AddHttpClientInstrumentation()
                     .AddRedisInstrumentation()
                     .ConfigureRedisInstrumentation((sp, i) =>
                     {
-                        var cache = (RedisCache)sp.GetRequiredService<IDistributedCache>();
-                        i.AddConnection(cache.GetConnection());
+                        var multiplexer = sp.GetRequiredService<IConnectionMultiplexer>();
+                        i.AddConnection(multiplexer);
                     })
                     .AddSource(nameof(Worker))
                     .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService("App4"))
